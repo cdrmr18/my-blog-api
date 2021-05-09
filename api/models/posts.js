4cac7a9dae9b65040b2da50e04f8e25a1790e431
Kathy Tavia <kathy.tavia@gmail.com>
2021-05-09 14:45:53 +0200

    Reading data from JSON file in NodeJS

--- api/models/posts.js
+++ api/models/posts.js
@@ -1,15 +1,28 @@
+const PATH = "./data.json";
+const fs = require('fs');
+
 class Post
 {
     get() {
         // get posts
+        return this.readData();
     }
 
     getSingleBlog() {
         // get one post
-        
+
     }
 
     add() {
         // add new post
     }
+
+    readData() {
+        // import data.json function then export it to be used
+        let data = fs.readFileSync(PATH);
+        let posts = JSON.parse(data);
+        return posts;
+    }
 }
+
+module.exports = Post
